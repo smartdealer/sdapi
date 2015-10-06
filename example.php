8d71cd53f1e94391c0c235bf70a41e8bd74aa6d0
Patrick Otto <patrick@siteguia.com>
2015-10-06 17:54:21 -0300

    Arquivo de exemplo: update

--- example.php
+++ example.php
@@ -1,20 +1,34 @@
 <?php
 
+// incluse API class
 include 'sdapi.class.php';
 
+// auth settings
 $sdl = 'http://localhost/sdsweb/webservice/rest';
-
 $usr = 'teste';
 $pwd = '56e893aebac5c5e549997b9a61ccf3f76bfe2580';
 
+# OR
+
+$sdl = 'prima';
+$usr = 'primafiat';
+$pwd = '80f2be7cc42dc32000730a35079226da65e3142f';
+
+// instace SD Api
 $api = new Smart\Api($sdl, $usr, $pwd);
 
-$response = $api->get('/connect/pack/27');
+// call Restful route
+$response = $api->get('/config/affiliates/');
 
+// collect erros
 $sts = $api->getError();
 
-if ($sts)
-    die($sts);
-
+// if error, show and stop
+if ($sts){
+    var_dump($sts);
+    die;
+}
+    
+// debug response
 echo "<pre>";
 var_dump($response);
