a3b9ffd4a4df6d89d4034c9ed60ab9c046553f21
phpenterprise <patrick@phpenterprise.com>
2017-03-30 16:46:00 -0300

    Novos recursos

--- public/example.php
+++ public/example.php
@@ -8,12 +8,6 @@ $sdl = 'http://localhost/sdsweb/webservice/rest';
 $usr = 'teste';
 $pwd = '56e893aebac5c5e549997b9a61ccf3f76bfe2580';
 
-# OR
-
-$sdl = 'grupotoniello';
-$usr = 'click';
-$pwd = '58c927d49befcccfdbdc03a09a903b073f3cf092';
-
 // instace SD Api
 $api = new Smart\Api($sdl, $usr, $pwd, array('debug' => false, 'output_format' => 1));
 
