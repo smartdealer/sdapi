f7aa5432cd259e291fc24b59e8140edc302bb951
phpenterprise <patrick@phpenterprise.com>
2017-05-17 07:56:00 -0300

    Novos métodos da versão 1.3, integrador

--- public/example.php
+++ public/example.php
@@ -14,11 +14,6 @@ $sdl = 'instancia';
 $usr = 'teste';
 $pwd = '56e893aebac5c5e549997b9a61ccf3f76bfe2580';
 
-$sdl = 'http://localhost/sdsweb/webservice/rest/';
-$usr = 'brsocial';
-$pwd = '87d2ad4299ebba438f727917e2744fa06e5008c0';
-$sdl = 'api';
-
 // instace SD Api
 $api = new Smart\Api($sdl, $usr, $pwd, array('debug' => false, 'output_format' => 1));
 
